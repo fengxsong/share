42e3e9d5eadd6f3b89911d2f154a1146a6b6e014
fengxsong <fengxsong@outlook.com>
2015-12-31 17:58:53 +0800

    v0.2

--- scripts/test_pyinotify.py
+++ scripts/test_pyinotify.py
@@ -2,11 +2,14 @@
 # -*- coding:utf-8 -*-
 
 import os
+import time
 import fnmatch
 import re
 import logging
 import yaml
 import pyinotify
+import redis
+from subprocess import call
 
 _DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 _LOGGER = logging.getLogger(__name__)
@@ -21,10 +24,17 @@ def _configure_logging():
 
 
 class EventHandler(pyinotify.ProcessEvent):
-    def my_init(self, base_dir, ignore):
+
+    def my_init(self, base_dir, exclusion, target, password_file, redis_c):
         self.abs_dir = os.path.abspath(base_dir)
-        self.ssw = ignore['startswith'].split(',')
-        self.esw = ignore['endswith'].split(',')
+        self.ssw = exclusion['startswith']
+        self.esw = exclusion['endswith']
+        self.targets = target
+        self.password_file = password_file
+        pool = redis.ConnectionPool(host=redis_c['host'], port=redis_c[
+                                    'port'], db=redis_c['db'])
+        self.redis_conn = redis.Redis(connection_pool=pool)
+        self.pipe = self.redis_conn.pipeline()
 
     def filter_swap(self, fn):
         if re.match(r'.(.*).sw*', fn):
@@ -41,43 +51,57 @@ class EventHandler(pyinotify.ProcessEvent):
         if self.notifier_filter(event):
             super(EventHandler, self).__call__(event)
 
+    def comfirm_rsync(self, fn, status_code):
+        self.pipe.hset(fn.pathname, 'timestamp', time.time()).hset(
+            fn.pathname, 'status_code', status_code).execute()
+        for target in self.targets:
+            call(["rsync", "-avH", "--delete", "--progress",
+                  "--password-file={0}".format(self.password_file), fn.pathname, target])
+        self.pipe.hset(fn.pathname, 'rsync', 1).execute()
+
     def process_IN_ATTRIB(self, event):
-        # do something.
         _LOGGER.info("metadata changed:%s", event.pathname)
 
     def process_IN_CREATE(self, event):
-        # do something.
         _LOGGER.info("creating:%s", event.pathname)
+        if not self.redis_conn.hgetall(event.pathname):
+            self.comfirm_rsync(event, 1)
 
     def process_IN_MODIFY(self, event):
-        # do something.
         _LOGGER.info("modify:%s", event.pathname)
+        if time.time() - float(self.redis_conn.hget(event.pathname, 'timestamp')) >= 3:
+            self.comfirm_rsync(event, 2)
 
     def process_IN_DELETE(self, event):
-        # do something.
         _LOGGER.info("delete:%s", event.pathname)
+        self.redis_conn.delete(event.pathname)
 
     def process_IN_MOVED_FROM(self, event):
-        # do something.
         _LOGGER.info("move:%s", event.pathname)
 
     def process_IN_MOVED_TO(self, event):
-        # do something.
         _LOGGER.info("move to:%s", event.pathname)
 
 
-def monitor(path, ignore):
+def monitor(source, exclusion, target, password_file, redis_c):
     wm = pyinotify.WatchManager()
-    handler = EventHandler(base_dir=path, ignore=ignore)
+    handler = EventHandler(
+        base_dir=source, exclusion=exclusion, target=target, password_file=password_file, redis_c=redis_c)
     notifier = pyinotify.Notifier(wm, default_proc_fun=handler)
-    wm.add_watch(path, pyinotify.ALL_EVENTS, rec=True, auto_add=True)
-    print '==> Start monitoring %s (type c^c to exit)' % path
+    wm.add_watch(source, pyinotify.ALL_EVENTS, rec=True, auto_add=True)
+    print '==> Start monitoring %s (type c^c to exit)' % source
     notifier.loop()
 
 
 if __name__ == '__main__':
     config = yaml.load(file('config.yml'))
-    path = config['path']
-    ignore = config['ignore']
+    source = config['source']
+    exclusion = config['exclusion']
+    target = config['target']
+    password_file = config['password_file']
+    if int(oct(os.stat(password_file).st_mode)[-3:]) > 600:
+        import stat
+        os.chmod(password_file, stat.S_IREAD + stat.S_IWRITE)
+    redis_c = config['redis']
     _configure_logging()
-    monitor(path, ignore)
+    monitor(source, exclusion, target, password_file, redis_c)
